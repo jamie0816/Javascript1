da0a5c9ff8b40d224b52fe9daea64ef3f568c3ee
jamie0816 <jamie020816@gmail.com>
2015-09-16 16:58:55 -1000

    9/16 2
    ddd

--- app1.js
+++ app1.js
@@ -1,7 +1,22 @@
 var press = function() {
 	alert("I'll burst your computer!");
 }
-var ask = function() {
-	var name = prompt("What is your favorite app??");
-	alert("No, "name + " is really bad. Use other one!");
+var food = function() {
+	var name = prompt("What's Jamie's favorite food??");
+	if (name === "mochi icecream"){
+		alert("Great Job!");
+	}
+	else{
+		alert("Try again...");
+	}
+	
+}
+var animal = function() {
+	var animal = prompt("What's Jamie's favorite animal??")
+	if (animal === "turtle"){
+		alert("Awesome!");
+	}
+	else{
+		alert("Don't cry!!")
+	}
 }
