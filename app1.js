5958120a345b18c34a39ca99bb9006a525cc1f65
jamie0816 <jamie020816@gmail.com>
2015-09-16 16:34:58 -1000

    javascript9/16
    9/16 we did alert box

---
+++ app1.js
@@ -0,0 +1,7 @@
+var press = function() {
+	alert("I'll burst your computer!");
+}
+var ask = function() {
+	var name = prompt("What is your favorite app??");
+	alert("No, "name + " is really bad. Use other one!");
+}
